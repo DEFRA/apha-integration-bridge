9ddeff9982843e7596e167a752de9e7c024deb06
David Kelley <david.james.kelley@gmail.com>
2025-06-02 09:20:50 +0100

    docs: added additionaljsdocs and typings for existing functions

--- src/common/helpers/start-server.js
+++ src/common/helpers/start-server.js
@@ -3,6 +3,15 @@ import { config } from '../../config.js'
 import { createServer } from '../../server.js'
 import { createLogger } from './logging/logger.js'
 
+/**
+ * Creates and starts a Hapi server instance.
+ *
+ * If server creation or startup fails the error is logged and the
+ * function resolves to `undefined`.
+ *
+ * @returns {Promise<import('@hapi/hapi').Server | undefined>} The running
+ * server or `undefined` when startup fails.
+ */
 async function startServer() {
   let server
 
