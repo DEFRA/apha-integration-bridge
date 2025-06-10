8213e5b00450c43882eebf44fa83c568ae217d44
David Kelley <david.james.kelley@gmail.com>
2025-06-10 16:43:22 +0100

    test(oracledb): improved testcontainer setup across tests

--- src/test/oracledb.js
+++ src/test/oracledb.js
@@ -1,33 +1,37 @@
 import oracledb from 'oracledb'
-import { beforeAll, afterAll } from '@jest/globals'
+import { beforeAll } from '@jest/globals'
 import { GenericContainer, Wait } from 'testcontainers'
 
+let container
+
 export const createOracleDbTestContainer = () => {
-  /**
-   * create a new test container for oracledb
-   *
-   * @note the docker daemon needs to have a valid login for oracle container registry
-   */
-  const container = new GenericContainer(
-    'container-registry.oracle.com/database/free:latest'
-  )
-    .withExposedPorts(1521)
+  if (!container) {
     /**
-     * run the necessary startup sql scripts
+     * create a new test container for oracledb
+     *
+     * @note the docker daemon needs to have a valid login for oracle container registry
      */
-    .withCopyDirectoriesToContainer([
-      {
-        source: './compose/oracledb',
-        target: '/opt/oracle/scripts/startup'
-      }
-    ])
-    .withEnvironment({
-      ORACLE_PW: 'letmein'
-    })
-    .withStartupTimeout(30_000)
-    .withWaitStrategy(
-      Wait.forLogMessage('DONE: Executing user defined scripts')
+    container = new GenericContainer(
+      'container-registry.oracle.com/database/free:latest'
     )
+      .withExposedPorts(1521)
+      /**
+       * run the necessary startup sql scripts
+       */
+      .withCopyDirectoriesToContainer([
+        {
+          source: './compose/oracledb',
+          target: '/opt/oracle/scripts/startup'
+        }
+      ])
+      .withEnvironment({
+        ORACLE_PW: 'letmein'
+      })
+      .withStartupTimeout(30_000)
+      .withWaitStrategy(
+        Wait.forLogMessage('DONE: Executing user defined scripts')
+      )
+  }
 
   /**
    * @type {Promise<import('testcontainers').StartedTestContainer>}
@@ -37,12 +41,6 @@ export const createOracleDbTestContainer = () => {
   beforeAll(async () => {
     await started
   }, 60_000)
-
-  afterAll(async () => {
-    const { stop } = await started
-
-    await stop()
-  })
 
   /**
    * @returns {Promise<Object>} the configuration for the oracledb test container
