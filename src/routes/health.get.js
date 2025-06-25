c1e3e7d46a0f7f84b0198fdc6bced5287fb6c34f
David Kelley <david.james.kelley@gmail.com>
2025-06-25 15:27:05 +0100

    fix: ensure health endpoint is unauthenticated

--- src/routes/health.get.js
+++ src/routes/health.get.js
@@ -1,1 +1,8 @@
+/**
+ * @type {import('@hapi/hapi').ServerRoute['options']}
+ */
+export const options = {
+  auth: false
+}
+
 export const handler = (_request, h) => h.response({ message: 'success' })
