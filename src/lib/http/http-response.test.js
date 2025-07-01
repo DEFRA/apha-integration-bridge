a73595e099f4f47a80cc5f2c930bdc05b6d2de7f
David Kelley <david.james.kelley@gmail.com>
2025-07-01 13:42:11 +0100

    feat: HTTPResponse class (#10)
    * feat: HTTPResponse class
    
    * fix: updated response body

---
+++ src/lib/http/http-response.test.js
@@ -0,0 +1,50 @@
+import { describe, test, expect } from '@jest/globals'
+import { HTTPResponse } from './http-response.js'
+
+describe('HTTPResponse', () => {
+  test('should correctly assign type, id, and attributes', () => {
+    const type = 'card'
+    const id = 'abc123'
+    const attributes = { name: 'Pikachu', power: 9001 }
+
+    const response = new HTTPResponse(type, id, attributes)
+
+    expect(response.type).toBe(type)
+    expect(response.id).toBe(id)
+    expect(response.attributes).toEqual(attributes)
+  })
+
+  test('toJSON() should return properly structured object', () => {
+    const response = new HTTPResponse('user', 42, { username: 'alice' })
+
+    expect(response.toJSON()).toEqual({
+      data: {
+        type: 'user',
+        id: 42,
+        username: 'alice'
+      }
+    })
+  })
+
+  test('should handle null attributes', () => {
+    const response = new HTTPResponse('item', 'xyz', null)
+
+    expect(response.toJSON()).toEqual({
+      data: {
+        type: 'item',
+        id: 'xyz'
+      }
+    })
+  })
+
+  test('should handle undefined id and attributes', () => {
+    const response = new HTTPResponse('thing', undefined, undefined)
+
+    expect(response.toJSON()).toEqual({
+      data: {
+        type: 'thing',
+        id: undefined
+      }
+    })
+  })
+})
