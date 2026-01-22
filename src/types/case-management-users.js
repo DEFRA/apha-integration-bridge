e2c7ba41816504b7ca14eafa864b22903f1594b7
mukeshrawat02 <mukeshrawat02@gmail.com>
2026-01-22 10:07:18 +0000

    Implemented user existence check in Salesforce with validation and retry logic

---
+++ src/types/case-management-users.js
@@ -0,0 +1,21 @@
+import Joi from 'joi'
+
+import { LinksReference } from './links.js'
+
+const CaseManagementUserData = Joi.object({
+  type: Joi.string()
+    .valid('case-management-user')
+    .required()
+    .label('Case Management User Type')
+    .description(
+      'The "type" value will be "case-management-user" for this endpoint.'
+    ),
+  id: Joi.string().required().label('User ID')
+})
+
+export const CaseManagementUserReference = Joi.object({
+  data: CaseManagementUserData.required(),
+  links: LinksReference
+})
+
+export const CaseManagementUser = CaseManagementUserData
