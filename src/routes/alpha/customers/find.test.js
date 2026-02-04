f09f71a51aaaef2298755e9d868dd15c87412136
Hugh F D Jackson <hugh@hughfdjackson.com>
2026-02-04 08:32:42 +0000

    DSFAAP-2184: add basic customer sketch

---
+++ src/routes/alpha/customers/find.test.js
@@ -0,0 +1,133 @@
+import Hapi from '@hapi/hapi'
+import { test, expect, describe } from '@jest/globals'
+
+import route from './find.js'
+import { customer1, customer2 } from './find.mocks.js'
+
+const path = '/customers/find'
+
+describe('customers/find', () => {
+  const server = Hapi.server()
+
+  server.route({
+    ...route,
+    path,
+    method: 'POST'
+  })
+
+  test('returns all matching ids', async () => {
+    const queryParams = new URLSearchParams({
+      page: '1',
+      pageSize: '10'
+    })
+    const url = `${path}?${queryParams.toString()}`
+    const response = await server.inject({
+      method: 'POST',
+      payload: {
+        ids: [customer1.id, customer2.id]
+      },
+      url
+    })
+
+    expect(response.result).toEqual({
+      data: [customer1, customer2],
+      links: {
+        self: url,
+        next: null,
+        prev: null
+      }
+    })
+  })
+
+  test('returns an empty array for no matches', async () => {
+    const queryParams = new URLSearchParams({
+      page: '1',
+      pageSize: '10'
+    })
+    const url = `${path}?${queryParams.toString()}`
+    const response = await server.inject({
+      method: 'POST',
+      payload: {
+        ids: ['missing', 'id']
+      },
+      url
+    })
+
+    expect(response.result).toEqual({
+      data: [],
+      links: {
+        self: url,
+        next: null,
+        prev: null
+      }
+    })
+  })
+
+  test('returns holdings in order requested', async () => {
+    const queryParams = new URLSearchParams({
+      page: '1',
+      pageSize: '10'
+    })
+    const url = `${path}?${queryParams.toString()}`
+    const response = await server.inject({
+      method: 'POST',
+      payload: {
+        ids: [customer2.id, customer1.id]
+      },
+      url
+    })
+
+    expect(response.result).toEqual({
+      data: [customer2, customer1],
+      links: {
+        self: url,
+        next: null,
+        prev: null
+      }
+    })
+  })
+
+  test('returns holdings paginated', async () => {
+    const queryParams = new URLSearchParams({
+      page: '1',
+      pageSize: '1'
+    })
+    const url = `${path}?${queryParams.toString()}`
+    const firstResponse = await server.inject({
+      method: 'POST',
+      payload: {
+        ids: [customer2.id, customer1.id]
+      },
+      url
+    })
+
+    const nextQueryParams = new URLSearchParams(queryParams.toString())
+    nextQueryParams.set('page', '2')
+
+    expect(firstResponse.result).toEqual({
+      data: [customer2],
+      links: {
+        self: url,
+        next: `${path}?${nextQueryParams.toString()}`,
+        prev: null
+      }
+    })
+
+    const secondResponse = await server.inject({
+      method: 'POST',
+      payload: {
+        ids: [customer2.id, customer1.id]
+      },
+      url: firstResponse.result.links.next
+    })
+
+    expect(secondResponse.result).toEqual({
+      data: [customer1],
+      links: {
+        self: `${path}?${nextQueryParams.toString()}`,
+        next: null,
+        prev: url
+      }
+    })
+  })
+})
--- src/types/alpha/customers.js
+++ src/types/alpha/customers.js
@@ -1,6 +1,61 @@
-import { relationshipToOne } from './helpers.js'
+import Joi from 'joi'
+
+import { baseData, relationshipToMany, relationshipToOne } from './helpers.js'
+
+// const ContactDetails = Joi.object({
+//   fullName: Joi.string().allow(null).label('Full name'),
+//   emailAddress: Joi.string().allow(null).label('Email address'),
+//   phoneNumber: Joi.string().allow(null).label('Phone number')
+// }).required()
+//
+// const BusinessContactDetails = Joi.object({
+//   primary: ContactDetails,
+//   secondary: ContactDetails
+// })
 
 export const CustomerRelationship = relationshipToOne({
   plural: 'customers',
   singular: 'customer'
 })
+
+export const CustomersData = baseData({
+  plural: 'customers',
+  singular: 'customer'
+})
+
+export const CustomersRelationships = Joi.object({
+  srabpiPlant: relationshipToMany({
+    singular: 'srabpi-plant',
+    plural: 'srabpi-plants'
+  })
+}).required()
+
+// export const BusinessCustomersData = CustomersData.keys({
+//   subType: Joi.string().required().valid('ORGANISATION'),
+//   organisationName: Joi.string().required().label('Organisation Name'),
+//   address: Address,
+//   contactDetails: BusinessContactDetails,
+//   relationships: CustomersRelationships
+// })
+
+// const IndividualContactDetails = Joi.object({
+//   type: Joi.string().valid('MOBILE', 'LANDLINE', 'EMAIL').required(),
+//   number: Joi.string().optional(),
+//   address: Joi.string().optional(),
+//   isPreferred: Joi.boolean()
+// })
+
+// export const IndividualCustomersData = CustomersData.keys({
+//   subType: Joi.string().required().valid('PERSON'),
+//   title: Joi.string().required(),
+//   firstName: Joi.string().required(),
+//   middleName: Joi.string().optional(),
+//   lastName: Joi.string().required(),
+//   addresses: Joi.array()
+//     .items(Address.keys({ isPreferred: Joi.boolean() }))
+//     .required(),
+//   contactDetails: Joi.array().items(IndividualContactDetails),
+//   relationships: CustomersRelationships
+// })
+//
+export const Customers = CustomersData
