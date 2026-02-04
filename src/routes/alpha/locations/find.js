2fa8567f9c6331c72a1d6ff523155db1a12d6ba6
Hugh F D Jackson <hugh@hughfdjackson.com>
2026-02-04 07:56:45 +0000

    DSFAAP-2184: added locations endpoint & better organised address data

---
+++ src/routes/alpha/locations/find.js
@@ -0,0 +1,70 @@
+import Joi from 'joi'
+import fs from 'node:fs'
+import path from 'node:path'
+
+import {
+  HTTPExceptionSchema,
+  HTTPException
+} from '../../../lib/http/http-exception.js'
+
+import { all } from './find.mocks.js'
+import { mockFindHandler } from '../helpers/find.js'
+import { PaginationSchema } from '../../../types/alpha/pagination.js'
+import { PaginatedLink } from '../../../types/alpha/links.js'
+import { Locations } from '../../../types/alpha/locations.js'
+
+const PostFindLocationsSchema = Joi.object({
+  data: Joi.array().items(Locations).required(),
+  links: PaginatedLink
+})
+  .description('Location Details')
+  .label('Find Location Response')
+
+const PostFindPayloadSchema = Joi.object({
+  ids: Joi.array().items(Joi.string()).required().label('Location ids')
+})
+
+const __dirname = new URL('.', import.meta.url).pathname
+
+/**
+ * @type {import('@hapi/hapi').ServerRoute['options']}
+ */
+const options = {
+  auth: false,
+  tags: ['api', 'locations'],
+  description: 'Retrieve locations by ids',
+  notes: fs.readFileSync(
+    path.join(decodeURIComponent(__dirname), 'find.md'),
+    'utf8'
+  ),
+  plugins: {
+    'hapi-swagger': {
+      id: 'locations-find',
+      security: [{ Bearer: [] }]
+    }
+  },
+  validate: {
+    payload: PostFindPayloadSchema,
+    headers: Joi.object({
+      accept: Joi.string()
+        .default('application/vnd.apha.1+json')
+        .description('Accept header for API versioning'),
+      'Content-Type': Joi.string().allow('application/json')
+    }).options({ allowUnknown: true }),
+    query: PaginationSchema,
+    failAction: HTTPException.failValidation
+  },
+  response: {
+    status: {
+      200: PostFindLocationsSchema,
+      '400-500': HTTPExceptionSchema
+    }
+  }
+}
+const handler = mockFindHandler(all)
+
+export default {
+  method: 'POST',
+  handler,
+  options
+}
