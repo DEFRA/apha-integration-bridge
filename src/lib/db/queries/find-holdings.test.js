7f22b6a82f83c56d1f609cfc5c73287eec340507
Jose Garcia <joseluisgraa@gmail.com>
2026-02-25 12:02:51 +0000

    Additional test for the query

--- src/lib/db/queries/find-holdings.test.js
+++ src/lib/db/queries/find-holdings.test.js
@@ -3,7 +3,15 @@ import { test, expect } from '@jest/globals'
 import { findHoldingsQuery } from './find-holdings.js'
 
 test('returns the expected query for valid parameters', () => {
-  const ids = ['01/000/0333']
+  const ids = ['11/111/1111']
+
+  const { sql } = findHoldingsQuery(ids)
+
+  expect(sql).toMatchSnapshot()
+})
+
+test('returns the expected query for multiple ids', () => {
+  const ids = ['11/111/1111', '22/222/2222']
 
   const { sql } = findHoldingsQuery(ids)
 
