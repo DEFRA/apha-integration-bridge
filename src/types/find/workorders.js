38124380352cd31c2ad4cbee3ed656ea66b8638d
Jose Garcia <joseluisgraa@gmail.com>
2026-03-03 13:08:07 +0000

    Added types for workorders

---
+++ src/types/find/workorders.js
@@ -0,0 +1,115 @@
+import Joi from 'joi'
+import { baseData, relationshipToOne, relationshipToMany } from './helpers.js'
+import { Activities } from './activities.js'
+import { HoldingsRelationship } from './holdings.js'
+
+/**
+ * @typedef {{
+ *  type: 'workorders'
+ *  id: string
+ *  activationDate: string
+ *  businessArea: string
+ *  workArea: string
+ *  country: string
+ *  aim: string
+ *  purpose: string
+ *  earliestActivityStartDate: string
+ *  species: string
+ *  activities: object[]
+ *  phase: string
+ *  relationships: {
+ *    customerOrOrganisation: object
+ *    holding: object
+ *    facilities: object[]
+ *    location: object
+ *    livestockUnits: object[]
+ *    }
+ * }} Workorders
+ */
+
+const WorkordersData = baseData({
+  plural: 'workorders',
+  singular: 'workorder'
+})
+
+const CustomerOrOrganisationRelationship = relationshipToOne(
+  Joi.alternatives(
+    baseData({
+      plural: 'customers',
+      singular: 'customer'
+    }),
+    baseData({
+      plural: 'organisations',
+      singular: 'organisation'
+    })
+  )
+)
+
+export const Workorders = WorkordersData.keys({
+  type: Joi.string().valid('workorders').required().label('Type'),
+  id: Joi.string().required().label('workorder'),
+  activationDate: Joi.string().required().allow(null).label('Activation Date'),
+  businessArea: Joi.string()
+    .required()
+    .allow(null)
+    .label('Business Area')
+    .description(
+      'The broad business area this workorder relates to - e.g. Disease Risk Reduction'
+    ),
+  workArea: Joi.string()
+    .required()
+    .allow(null)
+    .label('Work Area')
+    .description(
+      'The specific area this workorder relates to - e.g. Sheep Scab / TB'
+    ),
+  country: Joi.string().required().allow(null).label('Country'),
+  aim: Joi.string()
+    .required()
+    .allow(null)
+    .label('Aim')
+    .description(
+      'The broad outcome this workorder contributes to - e.g. Contain / Control / Eradicate Endemic Disease'
+    ),
+  purpose: Joi.string()
+    .required()
+    .allow(null)
+    .label('Purpose')
+    .description(
+      'The activity to be undertaken during the workorder - e.g. Sheep & Goat on Farm Survey'
+    ),
+  earliestActivityStartDate: Joi.string()
+    .required()
+    .allow(null)
+    .label('Start Date')
+    .description('The start date of the earliest activity'),
+  species: Joi.string().required().allow(null).label('Species'),
+  activities: Joi.array().items(Activities).required().label('Activities'),
+  phase: Joi.string().required().allow(null).label('Phase'),
+  relationships: Joi.object({
+    customerOrOrganisation: CustomerOrOrganisationRelationship.description(
+      'The individual or organisation that has the contact for the workorder'
+    ),
+    holding: HoldingsRelationship.description(
+      'Holding on which the workorder takes place'
+    ),
+    facilities: relationshipToMany(
+      baseData({
+        plural: 'facilities',
+        singular: 'facility'
+      })
+    ).description('Facilities involved in the workorder'),
+    location: relationshipToOne(
+      baseData({
+        plural: 'locations',
+        singular: 'location'
+      })
+    ).description('Locations involved during the workorder'),
+    livestockUnits: relationshipToMany(
+      baseData({
+        plural: 'animal-commodities',
+        singular: 'animal-commodity'
+      })
+    ).description('Livestock units to be inspected during the workorder')
+  }).required()
+})
