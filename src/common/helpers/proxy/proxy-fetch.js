081d1d0a396582cd31a80967566d13e8e8df3a42
mukeshrawat02 <mukeshrawat02@gmail.com>
2026-06-09 16:14:22 +0100

    Added timeout handling for JWKS fetch and added test

--- src/common/helpers/proxy/proxy-fetch.js
+++ src/common/helpers/proxy/proxy-fetch.js
@@ -1,15 +1,26 @@
 import { ProxyAgent } from 'undici'
 import { config } from '../../../config.js'
 
+/**
+ * Default timeout for JWKS fetch operations (in milliseconds)
+ */
+const JWKS_FETCH_TIMEOUT_MS = 5000
+
 export function proxyFetch(url, options) {
   const proxyUrlConfig = config.get('httpProxy') // bound to HTTP_PROXY
 
+  const timeoutSignal = AbortSignal.timeout(JWKS_FETCH_TIMEOUT_MS)
+  const signal = options?.signal
+    ? AbortSignal.any([options.signal, timeoutSignal])
+    : timeoutSignal
+
   if (!proxyUrlConfig) {
-    return fetch(url, options)
+    return fetch(url, { ...options, signal })
   }
 
   return fetch(url, {
     ...options,
+    signal,
     dispatcher: new ProxyAgent({
       uri: proxyUrlConfig,
       keepAliveTimeout: 10,
