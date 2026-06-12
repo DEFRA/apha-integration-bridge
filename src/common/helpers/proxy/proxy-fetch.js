1e874bb64e43c9e36259bc65f39d62c424a1e6b5
David Kelley <david.james.kelley@gmail.com>
2026-06-12 14:37:11 +0100

    fix(auth): test env resolved proxy:

--- src/common/helpers/proxy/proxy-fetch.js
+++ src/common/helpers/proxy/proxy-fetch.js
@@ -1,5 +1,4 @@
-import { ProxyAgent } from 'undici'
-import { config } from '../../../config.js'
+import { EnvHttpProxyAgent } from 'undici'
 
 /**
  * Default timeout for JWKS fetch operations (in milliseconds)
@@ -7,24 +6,10 @@ import { config } from '../../../config.js'
 const JWKS_FETCH_TIMEOUT_MS = 5000
 
 export function proxyFetch(url, options) {
-  const proxyUrlConfig = config.get('httpProxy') // bound to HTTP_PROXY
-
   const timeoutSignal = AbortSignal.timeout(JWKS_FETCH_TIMEOUT_MS)
   const signal = options?.signal
     ? AbortSignal.any([options.signal, timeoutSignal])
     : timeoutSignal
 
-  if (!proxyUrlConfig) {
-    return fetch(url, { ...options, signal })
-  }
-
-  return fetch(url, {
-    ...options,
-    signal,
-    dispatcher: new ProxyAgent({
-      uri: proxyUrlConfig,
-      keepAliveTimeout: 10,
-      keepAliveMaxTimeout: 10
-    })
-  })
+  return fetch(url, { ...options, signal, dispatcher: new EnvHttpProxyAgent() })
 }
